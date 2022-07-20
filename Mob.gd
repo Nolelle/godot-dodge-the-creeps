8549626340a9eed1edbe0fafb796053475f6075e
Edmund <eyu1@ualberta.ca>
2022-07-20 12:34:39 -0600

    finish player and mob scenes

---
+++ Mob.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	
+
+
+
+
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	pass # Replace with function body.
